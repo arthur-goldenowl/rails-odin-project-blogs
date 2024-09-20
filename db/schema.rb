b5dc97368937d789b2cbc7ebc9093b9da23ab216
arthur-goldenowl <arthur.trinh.goldenowl@gmail.com>
2024-09-20 11:29:17 +0700

    add status

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_20_030409) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_20_033806) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
   create_table "comments", force: :cascade do |t|
